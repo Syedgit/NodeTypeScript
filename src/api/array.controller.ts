4a6a2d9cf1e70f3ec3b7cc93b49daaf2c674dfd2
syed-hussain2 <syed.hussain2@cvshealth.com>
2020-04-30 13:42:06 -0400

    added filter & reduce logic

---
+++ src/api/array.controller.ts
@@ -0,0 +1,108 @@
+
+  
+import { Request, Response } from 'express';
+
+export class arrayController {
+  public static async filterById (req: Request, res: Response): Promise<any>  {
+    // sample data that should be pass to url http://localhost:9001/filter/byId/47
+    let response: any;
+    try {
+      const users = [
+        { id: 11, name: 'Adam', age: 23, group: 'editor' },
+        { id: 47, name: 'John', age: 28, group: 'admin' },
+        { id: 85, name: 'William', age: 34, group: 'editor' },
+        { id: 97, name: 'Oliver', age: 28, group: 'admin' }
+      ]
+      if(req.body.id){
+        response = users.filter((e: any) => {
+          if(req.body.id === e.id) {
+            return true;
+          }
+        }); 
+        res.status(200).send({
+          success: true,
+         data: response
+        })
+      }
+      
+    } catch (err) {
+      res.status(500).send({
+        success: false,
+        message: err.toString(),
+        data: null
+      });
+    }
+  };
+  public static async reduceArray (req: Request, res: Response): Promise<any>  {
+    // sample data that should be pass to url http://localhost:9001/reduce
+    try {
+      const arr = [
+        {id: "47", value: "test1", count: 21},
+        {id: "47", value: "test2", count: 87},
+        {id: "87", value: "Donald", count: 1},
+        {id: "65", value: "Jim", count: 1}
+      ];
+      // Desired output 
+      /*
+         "data": [
+        {
+            "id": "47",
+            "data": [
+                {
+                    "value": "test1",
+                    "count": 21
+                },
+                {
+                    "value": "test2",
+                    "count": 87
+                }
+            ]
+        },
+        {
+            "id": "87",
+            "data": [
+                {
+                    "value": "Donald",
+                    "count": 1
+                }
+            ]
+        },
+        {
+            "id": "65",
+            "data": [
+                {
+                    "value": "Jim",
+                    "count": 1
+                }
+            ]
+        }
+    ]*/
+      const result = arr.reduce((acc, d) => {
+        const found = acc.find(a => a.id === d.id);
+        //const value = { name: d.name, val: d.value };
+        const value = { value: d.value, count: d.count }; // the element in data property
+        if (!found) {
+          //acc.push(...value);
+          acc.push({id:d.id, data: [value]}) // not found, so need to add data property
+        }
+        else {
+          //acc.push({ name: d.name, data: [{ value: d.value }, { count: d.count }] });
+          found.data.push(value) // if found, that means data property exists, so just push new element to found.data.
+        }
+        return acc;
+      }, []);
+        res.status(200).send({
+          success: true,
+         data: result
+        })
+      
+    } catch (err) {
+      res.status(500).send({
+        success: false,
+        message: err.toString(),
+        data: null
+      });
+    }
+  };
+}
+
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,10 @@
 
 import {UserController} from './api/user/user.controller';
+import {arrayController} from './api/array.controller'
 export class RegisterRouteClass {
     public RegisterRoutes(app: any) {
+        app.post('/filter/byId', arrayController.filterById);
+        app.post('/reduce', arrayController.reduceArray);
         app.post('/user', UserController.findAll);
         // you have to pass id as http://localhost.com/getUser?id=23
         app.get('/getUser', UserController.findOne);
